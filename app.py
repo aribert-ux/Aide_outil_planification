bf1198cc91d6513083e4c87b6851f6ac25fa9fe3
aribert-ux <aribert@auchan.fr>
2026-07-28 16:14:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -349,7 +349,7 @@ if page == "🏠 Accueil":
             <div class="kpi-label">Types de marchandises</div>
         </div>""", unsafe_allow_html=True)    
         with col3:
-        st.markdown("""
+            st.markdown("""
         <div class="kpi-card">
             <div class="kpi-value">33 UT</div>
             <div class="kpi-label">Capacité max remorque</div>
